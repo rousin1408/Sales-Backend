a2760b210621b8f2dee24026e3cc9ecbea895573
Gerry Guinardi <73393827+rousin1408@users.noreply.github.com>
2024-10-22 00:13:36 +0700

    push port

--- src/main/java/com/example/Sales_Backend/SalesBackendApplication.java
+++ src/main/java/com/example/Sales_Backend/SalesBackendApplication.java
@@ -3,9 +3,10 @@ package com.example.Sales_Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SalesBackendApplication {
-
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(SalesBackendApplication.class, args);
 	}
